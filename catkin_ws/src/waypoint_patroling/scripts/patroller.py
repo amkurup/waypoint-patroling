8d7d76e48ce21237de2d000e4ead1b2977e79179
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-02-06 11:10:59 -0500

    Made modifications to map and waypoint coordinates in script

--- catkin_ws/src/waypoint_patroling/scripts/patroller.py
+++ catkin_ws/src/waypoint_patroling/scripts/patroller.py
@@ -7,33 +7,33 @@ from move_base_msgs.msg	import MoveBaseAction, MoveBaseGoal
 
 coordinates = [[0 for x in range(3)] for y in range(7)]
 coordinates[0][0] = -3
-coordinates[0][1] = -2
+coordinates[0][1] = -1
 coordinates[0][2] = 1
-coordinates[1][0] = 8
-coordinates[1][1] = -3
+coordinates[1][0] = 7
+coordinates[1][1] = -2
 coordinates[1][2] = 1
 coordinates[2][0] = 1
 coordinates[2][1] = -4
 coordinates[2][2] = 1
-coordinates[3][0] = 3
-coordinates[3][1] = 8
+#coordinates[3][0] = 3
+#coordinates[3][1] = 8
+#coordinates[3][2] = 1
+coordinates[3][0] = -3
+coordinates[3][1] = 3
 coordinates[3][2] = 1
-coordinates[4][0] = -2
-coordinates[4][1] = 2
+coordinates[4][0] = -5
+coordinates[4][1] = 7
 coordinates[4][2] = 1
-coordinates[5][0] = -5
-coordinates[5][1] = 7
+coordinates[5][0] = 8
+coordinates[5][1] = -8
 coordinates[5][2] = 1
-coordinates[6][0] = 8
-coordinates[6][1] = -8
-coordinates[6][2] = 1
 
 def patroller():
     mvbs = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     mvbs.wait_for_server()
     rospy.loginfo("Waiting for the move_base action server to come up")
     i = 0
-    #backtrack = False
+    backtrack = False
     while not rospy.is_shutdown():
         pt = MoveBaseGoal()
         pt.target_pose.header.frame_id = 'map'
@@ -45,34 +45,25 @@ def patroller():
         pt.target_pose.pose.orientation.z = 0
         pt.target_pose.pose.orientation.w = coordinates[i][2]
 
-       # while not rospy.is_shutdown():
         mvbs.send_goal(pt)
         mvbs.wait_for_result()
-        #if backtrack == False:
-        if i >= 6:
-            print "in > 6 block"
-            #backtrack = True
-            #i -= 1
-            i = 0
-            print "i is now: %d" % i
+        if backtrack == False:
+            if i >= 6:
+                backtrack = True
+                i -= 1
+            else:
+                i += 1
+            print "i: %d" % i
         else:
-            i += 1
-            print "increment to %d" % i
-        print "i: %d" % i
-       # else:
-       #     if i >= 6:
-       #         print "in > 6 block"
-
-       #         i = 0
-       #         print "i is now: %d" % i
-       #     else:
-       #         i -= 1
+            if i == 0:
+                backtrack = False
+                i = 0
+            else:
+                i -= 1
+            print "i is now: %d" % i
 if __name__ == "__main__":
     rospy.init_node("mapper")
     try:
-        for x in range(3):
-            for y in range(7):
-                print "i: %d, j: %d" % (x, y)
         patroller()
         rospy.spin()
     except rospy.ROSInterruptException:
