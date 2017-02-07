950200fc0b9870ab7204c05df3387ae413c54c85
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-02-07 16:55:06 -0500

    Updated waypoint coordinates and waypoint map

--- catkin_ws/src/waypoint_patroling/scripts/patroller.py
+++ catkin_ws/src/waypoint_patroling/scripts/patroller.py
@@ -14,77 +14,77 @@ coordinates = [[0 for x in range(3)] for y in range(8)]
 coordinates[0][0] = -3
 coordinates[0][1] = -1
 coordinates[0][2] = 1
-coordinates[1][0] = 7
+coordinates[1][0] = 6
 coordinates[1][1] = -2
 coordinates[1][2] = 1
 coordinates[2][0] = 1
 coordinates[2][1] = -4
 coordinates[2][2] = 1
-coordinates[3][0] = 3
+coordinates[3][0] = 2
 coordinates[3][1] = 8
 coordinates[3][2] = 1
 coordinates[4][0] = -3
-coordinates[4][1] = 3
+coordinates[4][1] = 5
 coordinates[4][2] = 1
-coordinates[5][0] = -5
-coordinates[5][1] = 7
+coordinates[5][0] = -6
+coordinates[5][1] = 6
 coordinates[5][2] = 1
 coordinates[6][0] = -8
-coordinates[6][1] = -2
+coordinates[6][1] = -3
 coordinates[6][2] = 1
-coordinates[7][0] = 8
-coordinates[7][1] = -8
+coordinates[7][0] = 6
+coordinates[7][1] = -9
 coordinates[7][2] = 1
 
 
 # Function decides initial waypoint based on robot's current position
 def setup_route(x, y):
     if ((x >= -6) and (x <= -2)) and ((y >= -3) and (y <= 0)):
-        # Set course for WP 1
+        # Set course for WP 0
         start = 0
     elif ((x >= 5) and ((y > -7) and (y <= 4))):
-        # Set course for WP 2
+        # Set course for WP 1
         start = 1
     elif ((x >= 1) and (x < 5)) and ((y >= -7) and (y <= -3)):
-        # Set course for WP 3
+        # Set course for WP 2
         start = 2
     elif ((x >= 5) and (y >= 4)) or (((x >= 1) and (x <= 5)) and (y >= 7)):
-        # Set course for WP 4
+        # Set course for WP 3
         start = 3
     elif ((x > -7) and (x <= -2)) and ((y >= 0) and (y <= 5)):
-        # Set course for WP 5
+        # Set course for WP 4
         start = 4
     elif ((x <= -1) and (y >= 5)) or ((x <= -7) and (y < -4)):
-        # Set course for WP 6
+        # Set course for WP 5
         start = 5
     elif ((x <= -7) and (y <= 4)) or ((x <= -4) and (y <= -3)):
-        # Set course for WP 7
+        # Set course for WP 6
         start = 6
     elif (x >= 1) and (y <= -7):
-        # Set course for WP 8
+        # Set course for WP 7
         start = 7
     elif ((x >= -2) and (x <= 5)) and ((y >= -3) and (y < 7)):
         # For deciding in area with angled obstacles
         diff = x - y
         print "diff: %d" % diff
         if (diff <= 1) and (diff >= -1):
-            # Set course for WP 1
+            # Set course for WP 0
             start = 0
         elif (diff >= 2):
-            # set course for WP 2 or 3
+            # set course for WP 1 or 2
             start = 1
         else:
-            # Set course for WP 4 or 5
+            # Set course for WP 3 or 4
             start = 3
     elif ((x <= 1) and (x >= -4)) and (y <= -3):
         # For deciding in area with angled obstacle
         diff = x - (-1 * y)
         print "diff: %d" % diff
         if (diff >= -5):
-            # Set course for WP 3
+            # Set course for WP 2
             start = 2
         else:
-            # Set course for WP 8
+            # Set course for WP 7
             start = 7
     else:
         rospy.loginfo("Assert")
