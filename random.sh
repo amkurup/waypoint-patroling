d5b3d52070ea4182e547c48e293d340c91a1b6c4
amkurup <amkurup@mtu.edu>
2017-02-07 17:33:32 -0500

    updating random spawn script

--- random.sh
+++ random.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# script to generate random points and spawn the jackal
+
+# Intro to Robotics - EE5900 - Spring 2017
+#          Assignment #4
+
+#       Project #4 Group #2
+#         Akhil (Team Lead)
+#            Philip
+#            Roger
+#
+# Revision: v1.1
+
+# define X and Y coordinate points
 point_x=0
 point_y=0
 
+# generate random data points within +- 7
 point_x=$((RANDOM % 8))
 point_y=$((RANDOM % 8))
 
@@ -12,10 +26,16 @@ let "point_y = $point_y - 5"
 echo "X location: $point_x"
 echo "Y location: $point_y"
 
+
+# rosbag_files
+# rm catkin_ws/src/waypoint_patroling/rosbag_files/*
+
+# make intermediate file for launch
 cp catkin_ws/src/waypoint_patroling/launch/waypoint_patroling_2.launch catkin_ws/src/waypoint_patroling/launch/waypoint_patroling_22.launch
 
-echo "roslaunch waypoint_patroling waypoint_patroling_22.launch spawn:="$point_x $point_y""
-#roslaunch waypoint_patroling waypoint_patroling_2.launch spawn:="$point_x $point_y"
+# replace the spawn locations with the random points
 sed -i 's/-x 0 -y 0/-x '$point_x' -y '$point_y'/g' catkin_ws/src/waypoint_patroling/launch/waypoint_patroling_22.launch
+echo "roslaunch waypoint_patroling waypoint_patroling_22.launch spawn:="$point_x $point_y""
 roslaunch waypoint_patroling waypoint_patroling_22.launch spawn:="$point_x $point_y"
+
 
