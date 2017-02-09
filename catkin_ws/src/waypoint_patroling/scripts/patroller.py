bf3e2bec92b6b6e14608389afb43737ecec5b278
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-02-08 23:47:36 -0500

    Coordinate offset in patroller.py not needed. Added gmapping images from patrol rosbag playback

--- catkin_ws/src/waypoint_patroling/scripts/patroller.py
+++ catkin_ws/src/waypoint_patroling/scripts/patroller.py
@@ -56,30 +56,6 @@ coordinates[7][2] = 1
 
 # Function decides initial waypoint based on robot's current position
 def setup_route(x, y):
-    coordinates[0][0] = -3 - x    # (1, -1) -> (-4, 0), (1, 1) -> (-4, -2)     (2, -4) -> (-5, 3), (2, 4) -> (-5, -5)
-    coordinates[0][1] = -1 - y    # (-1, -1) -> (-2, 0), (-1, 1) -> (-2, -2)   (-2, -4) -> (-1, 3), (-2, 4) -> (-1, 5)
-    coordinates[0][2] = 1         #
-    coordinates[1][0] = 6 - x     # (1, -1) -> (5, -1), (1, 1) -> (5, -3)      (2, -4) -> (4, 2), (2, 4) -> (4, -6)
-    coordinates[1][1] = -2 - y    # (-1, -1) -> (7, -1), (-1, 1) -> (7, -3)    (-2, -4) -> (8, 2), (-2, 4) -> (8, 6)
-    coordinates[1][2] = 1         #
-    coordinates[2][0] = 1 - x     #
-    coordinates[2][1] = -4 - y    #
-    coordinates[2][2] = 1         #
-    coordinates[3][0] = 2 - x     # (1, -1) -> (1, 9), (1, 1) -> (1, 7)
-    coordinates[3][1] = 8 - y     # (-1, -1) -> (3, 9), (-1, 1) -> (3, 7)
-    coordinates[3][2] = 1         #
-    coordinates[4][0] = -3 - x    # (1, -1) -> (-4, 5), (1, 1) -> (-4, 3)
-    coordinates[4][1] = 4 - y     # (-1, -1) -> (-2, 5), (-1, 1) -> (-2, 3)
-    coordinates[4][2] = 1         #
-    coordinates[5][0] = -6 - x    #
-    coordinates[5][1] = 6 - y     #
-    coordinates[5][2] = 1         #
-    coordinates[6][0] = -8 - x    #
-    coordinates[6][1] = -3 - y    #
-    coordinates[6][2] = 1         #
-    coordinates[7][0] = 6 - x     #
-    coordinates[7][1] = -9 - y    #
-    coordinates[7][2] = 1         #
 
     if ((x >= -6) and (x <= -2)) and ((y >= -3) and (y <= 0)):
         # Set course for WP 0
